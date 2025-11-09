0c0c97bb352c35aa3d4910f4ca634c6f917fa92d
vaxtalastus-web <vaxtalastus@gmail.com>
2025-11-09 12:42:20 +1100

    Update BetterAntiKickWitherror.lua

--- BetterAntiKickWitherror.lua
+++ BetterAntiKickWitherror.lua
@@ -1,8 +1,12 @@
 assert(hookmetamethod, "Your exploit does not support anti-kick")
 assert(getnamecallmethod, "Your exploit does not support anti-kick")
-hookmetamethod(game, "__namecall", function(self, ...)
-    local method = getnamecallmethod()
-    if method:lower() == "kick" then
-        return error("Attempt to call a nil value", 2)
-    end
-end)
+local old
+do
+	old = hookmetamethod(game, "__namecall", function(self, ...)
+		local method = getnamecallmethod()
+		if method:lower() == "kick" then
+			return error("Attempt to call a nil value", 2)
+		end
+		return old(self, ...)
+	end)
+end
